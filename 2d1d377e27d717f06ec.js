1081b758c050b2cd3ab4130516324191f8583cb8
mbahnunung <mbahnunung@users.noreply.github.com>
2020-03-25 07:44:14 +0700

    Update 2d1d377e27d717f06ec.js

--- 2d1d377e27d717f06ec.js
+++ 2d1d377e27d717f06ec.js
@@ -72,7 +72,7 @@ $('#top-4').click(function () {
         host: 'live.radiobintangtenggara.com',
         port: 8000,
         version: 2,
-        directStreamURL: 'https://hs.mbahnunungonline.net/stream/',
+        directStreamURL: 'https://radiofox.herokuapp.com/?q=http://live.radiobintangtenggara.com:8000/;',
         defaultArtwork: ['https://i.stack.imgur.com/LOa4W.png'],
         irrelevantWords: false,
         colors: 'dynamic',
