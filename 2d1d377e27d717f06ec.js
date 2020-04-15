8f522cc030d634c0ea540b39e52475de3ee85f0a
mbahnunung <mbahnunung@users.noreply.github.com>
2020-04-15 14:34:56 +0700

    Update 2d1d377e27d717f06ec.js

--- 2d1d377e27d717f06ec.js
+++ 2d1d377e27d717f06ec.js
@@ -69,10 +69,10 @@ $('#top-3').click(function () {
 // Handle: Example 4 click function
 $('#top-4').click(function () {
     $.kast({
-        host: 'live.us.bintangtenggarafm.com',
-        port: 8000,
+        host: 'live.bintangtenggarafm.com',
+        port: 9560,
         version: 2,
-        directStreamURL: 'https://castprox.herokuapp.com/http://live.us.bintangtenggarafm.com:8000/mbahnunungonline',
+        directStreamURL: 'https://castprox.herokuapp.com/http://live.bintangtenggarafm.com:9560/rbt',
         defaultArtwork: ['https://warningfm.github.io/v3/images/no-cover-large.png'],
         irrelevantWords: false,
         colors: 'dynamic',
