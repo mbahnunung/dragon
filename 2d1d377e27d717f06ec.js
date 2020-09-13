32478e7279f2ff7216e358de302880a06d679c1a
mbahnunung <mbahnunung@users.noreply.github.com>
2020-09-13 13:38:11 +0700

    Update 2d1d377e27d717f06ec.js

--- 2d1d377e27d717f06ec.js
+++ 2d1d377e27d717f06ec.js
@@ -22,8 +22,8 @@ $('#top-1').click(function () {
         host: 'live.prodjradio.net',
         port: 8000,
         protocol: 'http',
-        version: 2,
-        sid: 1,
+        version: 1,
+        //sid: 1,
         directStreamURL: 'https://live.prodjradio.net:8065/radio',
         defaultArtwork: ['no-cover-large.png'],
         irrelevantWords: false,
@@ -39,8 +39,8 @@ $('#top-2').click(function () {
         host: '87.98.216.129',
         port: 4638,
         protocol: 'http',
-        version: 2,
-        sid: 1,
+        version: 1,
+        //sid: 1,
         directStreamURL: 'https://www.rcast.net/proxy/152768.mp3',
         defaultArtwork: ['no-cover-large.png'],
         irrelevantWords: false,
@@ -58,6 +58,7 @@ $('#top-3').click(function () {
     $.kast({
         host: 'stream3.radiocenter.si',
         port: 8068,
+        version: 1,
         directStreamURL: 'https://center.mbahnunungonline.net/proxy/center2?mp=/center',
         defaultArtwork: ['https://warningfm.github.io/v3/images/1d205655ef29e14a8255c89fe2383a41.jpg'],
         irrelevantWords: false,
@@ -71,7 +72,7 @@ $('#top-4').click(function () {
     $.kast({
         host: 'live.sg.radiobintangtenggara.com',
         port: 9560,
-        version: 2,
+        version: 1,
         directStreamURL: 'https://www.rcast.net/proxy/159652',
         defaultArtwork: ['https://warningfm.github.io/v3/images/no-cover-large.png'],
         irrelevantWords: false,
@@ -87,6 +88,7 @@ $('#top-5').click(function () {
     $.kast({
         host: 'curiosity.shoutca.st',
         port: 8098,
+        version: 1,
         artwork: true,
         //directStreamURL: 'https://corsradio.herokuapp.com/?q=http://curiosity.shoutca.st:8098/stream',
         defaultArtwork: ['https://warningfm.github.io/v3/images/no-cover-large.png'],
@@ -103,7 +105,7 @@ $('#top-6').click(function () {
     $.kast({
         host: 'tgas.dyndns.org',
         port: 8010,
-        version: 2,
+        version: 1,
         protocol: 'http',
         //directStreamURL: 'https://clover-cors.herokuapp.com/?q=http://tgas.dyndns.org:8010/2',
         defaultArtwork: ['https://i.imgur.com/oDvLz7u.jpg'],
@@ -122,8 +124,8 @@ $('#top-7').click(function () {
     $.kast({
         host: 'sj128.hnux.com',
         port: 80,
-        version: 2,
-        sid: 1,
+        version: 1,
+        //sid: 1,
         protocol: 'http',
         //directStreamURL: 'https://sertanejapop.herokuapp.com/?q=http://sj128.hnux.com:80/stream',
         defaultArtwork: ['https://warningfm.github.io/v3/images/1d205655ef29e14a8255c89fe2383a41.jpg'],
@@ -140,6 +142,7 @@ $('#top-8').click(function () {
     $.kast({
         host: 'live.sg.mcfmbwi.com',
         port: 9720,
+        version: 1,
         //directStreamURL: 'https://corsradio.herokuapp.com/?q=http://live.sg.mcfmbwi.com:9720/mcfm',
         irrelevantWords: false,
         colors: 'dynamic',
@@ -154,7 +157,7 @@ $('#top-9').click(function () {
     $.kast({
         host: 'stream.ariegm.com',
         port: 8800,
-        version: 2,
+        version: 1,
         protocol: 'http',
         directStreamURL: 'https://us-server.radioidola.com:8880/stream',
         irrelevantWords: false,
@@ -172,8 +175,8 @@ $('#top-10').click(function () {
     $.kast({
         host: '144.76.106.52',
         port: 7000,
-        version: 2,
-        sid: 1,
+        version: 1,
+        //sid: 1,
         //directStreamURL: 'https://castprox.herokuapp.com/http://144.76.106.52:7000/progressive.aac',
         irrelevantWords: false,
         colors: {
@@ -191,7 +194,7 @@ $('#top-11').click(function () {
     $.kast({
         host: 'live.radiotawangalun.com',
         port: 8060,
-        sid: 1,
+        version: 1,
         protocol: 'http',
         //directStreamURL: 'https://corsradio.herokuapp.com/index.php?q=http://live.radiotawangalun.com:8060/rsta',
         irrelevantWords: false,
@@ -207,6 +210,7 @@ $('#top-12').click(function () {
     $.kast({
         host: 'diradio.biru.co.id',
         port: 8910,
+        version: 1,
         colors: 'dynamic',
         theme: 'dynamic',
         mobileCare: ['ultra', '4069px'], // the pixel number here is to force MobileCare to run on Desktops and Tablets too (Up to 4K Screens)
