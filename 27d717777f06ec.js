bc20a490f2ea2c57251b7f64ee7665411bdabc86
mbahnunung <mbahnunung@users.noreply.github.com>
2020-10-16 02:05:37 +0700

    Update 27d717777f06ec.js

--- 27d717777f06ec.js
+++ 27d717777f06ec.js
@@ -10,33 +10,19 @@
         port: 9560,
         protocol: 'https',
         version: 1,
-        sid: 1,
-        statsPath: 'stats',
         //directStreamURL: 'https://cast4.mbahnunungonline.net/proxy/mbahnunung55?mp=/stream',
-        autoUpdate: true,
-        autoPlay: true,
-        betaProxies: true,
-        mobileCare: false,
-        minimizeMaximize: false,
-        muteUnmute: false,
+        defaultArtwork: ['https://warningfm.github.io/v3/images/1d205655ef29e14a8255c89fe2383a41.jpg'],
         statusBar: false,
         startTemplate: 'maximized',
-        irrelevantWords: ['false'],
-        defaultArtwork: ['https://warningfm.github.io/v3/images/1d205655ef29e14a8255c89fe2383a41.jpg'],
-        offlineCheck: false,
         language: {
             offlineText: 'Temporarily Offline',
             playedText: '<font face="Georgia" color="blue">Wes Mari Diputer :</font>',
             unknownTrackText: 'Informasi & Solusi - c^o^d^e^l^i^s^t^.^c^c', 
             unknownArtistText: 'Radio Bintang Tenggara' 
-        }, 
-        overHTTPS: true,
+        },
         position: 'left',
-        container: 'body',
         colors: 'dynamic',
         theme: 'dark',
-        ui: 'colored',
-        played: false,
-        artwork: true,
-        continuous: false
-    })
+        autoPlay: true,
+        played: false
+      })
