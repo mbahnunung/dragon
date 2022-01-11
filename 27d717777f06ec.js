c0d44983df9a311180f476c196d8c841ed8d214d
mbahnunung <mbahnunung@users.noreply.github.com>
2022-01-11 19:14:58 +0700

    Update 27d717777f06ec.js

--- 27d717777f06ec.js
+++ 27d717777f06ec.js
@@ -10,7 +10,7 @@
         port: 9560,
         protocol: 'http',
         version: 1,
-        directStreamURL: 'https://ssg.streamingmurah.com:9560/rbt',
+        directStreamURL: 'https://sv.stri.my.id/radio/8360/rbtfm',
         defaultArtwork: ['https://raw.githubusercontent.com/warningfm/v3/master/images/1d205655ef29e14a8255c89fe2383a41.jpg'],
         statusBar: false,
         startTemplate: 'maximized',
